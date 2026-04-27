fecc589d609c3f0cc83b867efec2ecdbdcab6762
arRahat129 <rahashik129@gmail.com>
2026-04-27 16:50:50 +0600

    just updated the marquee data for breaking news

--- src/components/shared/BreakingNews.jsx
+++ src/components/shared/BreakingNews.jsx
@@ -1,29 +1,33 @@
+import { getNewsByCategoryId } from '@/lib/data';
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 
-const news = [
-    {
-        _id: "1", 
-        title: "Breaking News: Major Event Unfolds in the New York City",
-    },
-    {
-        _id: "2", 
-        title: "Breaking News: New Policy Announced by the Goverment",
-    },
-    {
-        _id: "3", 
-        title: "Breaking News: Sports Team Wins Championship -- The Football Breaks...",
-    },
-];
+// const news = [
+//     {
+//         _id: "1", 
+//         title: "Breaking News: Major Event Unfolds in the New York City",
+//     },
+//     {
+//         _id: "2", 
+//         title: "Breaking News: New Policy Announced by the Goverment",
+//     },
+//     {
+//         _id: "3", 
+//         title: "Breaking News: Sports Team Wins Championship -- The Football Breaks...",
+//     },
+// ];
 
-const BreakingNews = () => {
+
+const BreakingNews = async() => {
+    const news = await getNewsByCategoryId("01");
+
     return (
         <div className='flex justify-center items-center gap-4 bg-gray-200 py-4 px-2 container mx-auto'>
             <button className='btn bg-red-500 rounded-lg text-white'>Latest News</button>
-            <Marquee pauseOnHover={true} speed={100}>
+            <Marquee pauseOnHover={true} speed={50}>
                 {
                     news.map(n => {
-                        return <span className='mr-5' key={n._id}>{n.title}</span>
+                        return <span className='mx-10' key={n._id}><span className='text-red-500 font-semibold'>Breaking News:</span> {n.title}</span>
                     })
                 }
             </Marquee>
